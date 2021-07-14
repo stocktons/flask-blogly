3fb524b5c8ee2c43585ee3a40c3917ec443b8dcc
stocktons <sarah@thanksforallthe.fish>
2021-07-14 15:53:23 -0400

    created user, app, base template

--- models.py
+++ models.py
@@ -1,1 +1,30 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.Text,
+                     nullable=False)
+    last_name = db.Column(db.Text,
+                     nullable=False)
+    image_url = db.Column(db.Text, 
+                    nullable=False, 
+                    default='https://photolibrary.usap.gov/Tools/DrawImage.aspx?filename=emperor-penguin-noble.jpg')
+                    # can we restrict image size?
+                    
