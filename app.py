52e20c0b789c4ff3d684e3c6603070655a28e3c7
Mike Chang <mchang95@gmail.com>
2021-07-14 16:08:42 -0700

    adds templates and routes up to user details

--- app.py
+++ app.py
@@ -17,3 +17,55 @@ debug = DebugToolbarExtension(app)
 db.create_all()
 
 
+@app.route("/")
+def show_homepage():
+    """ Redirects to list of users. """
+
+    return redirect ("/users")
+
+
+
+@app.route("/users")
+def show_user_list(): 
+    """ Shows list of all users. """
+
+    users = User.query.all()
+
+    return render_template("user_listing.html", users = users)
+
+
+@app.route("/users/new")
+def show_add_user_form(): 
+    """ Shows form to add new user. """
+
+    return render_template ("new_user.html")
+
+
+@app.route("/users/new", methods=["POST"])
+def add_user(): 
+    """ Creates new user from input """
+
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    image_url = request.form['img-url']
+
+    user = User(
+                first_name=first_name,
+                last_name=last_name,
+                image_url=image_url
+                )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(f"/users/{user.id}")
+
+
+
+@app.route("/users/<int:user_id>")
+def show_user(user_id): 
+    """ Show info on a single user. """
+
+    user = User.query.get_or_404(user_id)
+    return render_template("user_detail.html", user=user)
+
